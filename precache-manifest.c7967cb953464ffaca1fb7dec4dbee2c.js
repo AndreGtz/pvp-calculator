be561635443474ba9955a527f89c34cd17d00f7f
andregtz <colpac64@gmail.com>
2020-09-22 00:57:42 -0500

    Updates

---
+++ precache-manifest.c7967cb953464ffaca1fb7dec4dbee2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4d205419425fd9ab58caac290c918a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75541b6076a372577a68",
+    "url": "/static/css/main.52c41aa6.chunk.css"
+  },
+  {
+    "revision": "175c184027a1c8a47807",
+    "url": "/static/js/2.b5ec2332.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b5ec2332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75541b6076a372577a68",
+    "url": "/static/js/main.e9a6de2d.chunk.js"
+  },
+  {
+    "revision": "ad5e10207db7abeef351",
+    "url": "/static/js/runtime-main.6dd243e7.js"
+  }
+]);
